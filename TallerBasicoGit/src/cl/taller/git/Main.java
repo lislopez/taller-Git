ab93baca642ff8e55d358c26f3affac90f1387be
lislopez <lislopez@alumnos.ubiobio.cl>
2014-04-07 13:17:22 -0700

    modifica en web

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Mi segundo commit ");
-		System.out.println("Linea agregada desde la web");
+		System.out.println("Linea agregada desde la web, modifica en web");
 		// TODO Auto-generated method stub
 
 	}
