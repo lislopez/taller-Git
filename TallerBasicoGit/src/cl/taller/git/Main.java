3a2d57d861abe00ff9065dad3519e5a02a4653e5
liss <liss@10.1.0.153>
2014-04-07 16:32:05 -0300

    Mi segundo commit

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Mi primer ");
+		System.out.println("Mi segundo commit ");
 		// TODO Auto-generated method stub
 
 	}
