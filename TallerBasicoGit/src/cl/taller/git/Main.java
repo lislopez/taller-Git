fa0d0b6d21fa91c8537ed7e4943940f41f676d9d
lislopez <lislopez@alumnos.ubiobio.cl>
2014-04-07 12:47:45 -0700

    Linea agregada por la web

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Mi segundo commit ");
+		System.out.println("Linea agregada desde la web");
 		// TODO Auto-generated method stub
 
 	}
